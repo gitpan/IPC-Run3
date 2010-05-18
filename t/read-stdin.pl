6a67bb6ed5df87479e0aec78fbd43ebc9a30b36d
Roderich Schupp <rschupp@cpan.org>
2010-05-18 16:56:16 +0000

    Import of RSCHUPP/IPC-Run3-0.043_03 from CPAN.
    gitpan-cpan-distribution: IPC-Run3
    gitpan-cpan-version:      0.043_03
    gitpan-cpan-path:         RSCHUPP/IPC-Run3-0.043_03.tar.gz
    gitpan-cpan-author:       RSCHUPP
    gitpan-cpan-maturity:     developer

--- t/read-stdin.pl
+++ t/read-stdin.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 use Test::More tests => 3;
+
+use blib;
 use IPC::Run3;
 
 sub read_stdin
@@ -11,27 +13,27 @@ sub read_stdin
     my ($nlines, $run3_after) = @_;
 
     # read lines from <>, calling run3() after line $run3_after
-    my $nread = 0;
+    my $lineno = 1;
     my $garbled;
     while (<>)
     {
 	chomp;
-	my $expected = "this is line $.";
-	$garbled = qq[line $nread: expected "$expected", got "$_"]
+	my $expected = "this is line $lineno";
+	$garbled = qq[line $.: expected "$expected", got "$_"]
 	    unless $expected eq $_ || $garbled;
-	$nread++;
+	$lineno++;
 
-	if ($nread == $run3_after)
+	if ($lineno == $run3_after)
 	{
 	    my ($out, $err);
 	    run3 [ $^X, "-e", "print qq[some\\nthing]" ], \undef, \$out, \$err;
 	    die "command failed" unless $? == 0;
-	    is $out, "some\nthing", "STDOUT captured correctly";
+	    is $out, "some\nthing", "STDOUT correctly captured";
 	}
     }
 
-    is $nread, $nlines, "STDIN truncated?";
-    ok !$garbled, "STDIN not garbled?"
+    is $lineno-1, $nlines, "STDIN not truncated";
+    ok !$garbled, "STDIN not garbled"
 	or diag $garbled;
 
     exit(0);
